2b6dfaa5c671ef7f08815ef813278c95aa437d3b
DeepthiGirijaThampi <deepthig01@gmail.com>
2025-08-20 20:42:51 -0500

    Final commit added the instructions as comment

--- beverage.js
+++ beverage.js
@@ -1,3 +1,12 @@
+// Beverage
+// Define a Beverage class with properties name, ounces, container, and isCold.
+// isCold should always be false at instantiation of an object.
+// container should be optional and default to "bottle".
+// Add a describe() method that prints a statement using the values stored in the object's other properties. The statement should read differently depending on whether the beverage is cold or not.
+// Add a refrigerate() method outside the constructor that changes the value of isCold and prints a notification.
+// Instantiate at least 3 different kinds of beverages with unique properties.
+// Use the describe() method to print descriptions of each beverage.
+// Use the refrigerate() method on one of the beverages, then call describe()  again.
 class Beverage{
     constructor(name,ounces,container = "bottle"){
         this.name = name;
